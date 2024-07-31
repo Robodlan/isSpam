7036373f8744ba4579c117f0f8cc8c643affe00c
Robodlan <azuldevs@zohomail.com>
2024-07-31 11:12:55 -0400

    Regex

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const text = document.getElementById("texterea")
 const result = document.getElementById("result")
 const btn = document.getElementById("btn")
 
-const isSpam = (msg) => false
+
+const helpRegex = /please help/i
+const isSpam = (msg) => msg.text(helpRegex)
+
 
 btn.addEventListener("click", () =>{
+    console.log(isSpam);
   if (text.value === "") {
     alert("Please enter a message.")
     return
   }
+  result.textContent = isSpam(text.value) ?  
+  "Oh no! This looks like a spam message." : 
+  "This message does not seem to contain any spam."
+  text.value = ""
+  
 })
