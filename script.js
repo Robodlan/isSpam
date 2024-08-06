c28abefc3c0eb7b6b30f2bbc1f86f0b6414afd90
Robodlan <azuldevs@zohomail.com>
2024-08-06 11:29:43 -0400

    script regex

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const btn = document.getElementById("btn")
 
 
 const helpRegex = /please help | assist me/i
-const dollarRegex = /dollars/i
-const denyList = [helpRegex, dollarRegex]
+const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i
+const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i
+const dearRegex = /(?:\s|^)d[e3][a4@]r fr[i1|][e3]nd(?:\s|$)/i
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex]
 const isSpam = (msg) => denyList.some(regex => regex.test(msg))
 
 btn.addEventListener("click", () =>{
