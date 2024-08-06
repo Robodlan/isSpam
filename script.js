a584cb9c9735ad167aac5532a51e56b3e8e4a99e
Robodlan <azuldevs@zohomail.com>
2024-08-06 08:57:47 -0400

    HTML

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const result = document.getElementById("result")
 const btn = document.getElementById("btn")
 
 
-const helpRegex = /please help/i
-const isSpam = (msg) => msg.text(helpRegex)
-
+const helpRegex = /please help | assist me/i
+const isSpam = (msg) => helpRegex.test(msg)
 
 btn.addEventListener("click", () =>{
-    console.log(isSpam);
+   console.log(isSpam);
   if (text.value === "") {
     alert("Please enter a message.")
     return
